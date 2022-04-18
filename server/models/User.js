edb6096713eda9f375b05987459066c0252951f7
iamashutoshp <ap1853177@gmail.com>
2022-04-18 21:29:24 +0530

    backend/feat: signIn functionality added || dotenv changes || user Model userName to name change

--- server/models/User.js
+++ server/models/User.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  userName: { type: String, required: true },
+  name: { type: String, required: true },
   emailId: { type: String, required: true },
   hashedPassword: { type: String, required: true },
 });
+
+UserSchema.static('findOneByEmailId', function(email) { return this.find({ emailId: email }); });
 
 const model = mongoose.model("User", UserSchema);
 
